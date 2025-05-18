a8eca2bcb093163d29d434e713e7aef862a215ad
micahtracht <micahtracht@gmail.com>
2025-05-17 20:18:08 -0400

    Refactored tabular Q-learning and changed config file. Also did minor refactoring in cartpoledqn to account for the changes to the config file.

--- CartPoleTabular.py
+++ CartPoleTabular.py
@@ -5,99 +5,11 @@ import gym
 from discretizer import Discretizer
 import matplotlib.pyplot as plt
 from typing import Sequence
+from config import Config
 
-# Create the environment using gym.make. This defines the place where the agent will trian, including:
-# the reward signal, possible actions, the state, and more.
-env = gym.make("CartPole-v1")
+# - Globals & Helpers -
 
-n_bins = 10
-n_angle_bins = 20 # refine this further because the system is very sensitive to the angle.
-bin_counts = [n_bins, n_bins, n_angle_bins, n_bins]
-# goes in cart pos, cart vel, pole angle, pole angular vel
-lower_bounds = np.array([-4.8, -5, -0.418, -5])
-upper_bounds = np.array([4.8, 5, 0.418, 5])
-# Create the state space, which is 10^4 x 2 = 20,000 states (n_bins^dim * num_actions)
-Q = np.zeros(bin_counts + [env.action_space.n], dtype='float')
-# make discretizer
-disc = Discretizer(bins_per_feature=bin_counts, lower_bounds=lower_bounds, upper_bounds=upper_bounds)
-
-env.action_space.n = 2
-alpha, alpha_min, alpha_decay = 0.3, 0.005, 0.999 # learning rate, how much do we update Q(S,A) each time. What to mult the learning rate by each time. Min value for learning rate.
-gamma = 0.99 # discount factor for future rewards, if gamma = 1 we have instability in stochastic environments, but gamma should be near 1 because we care about future rewards too
-epsilon, epsilon_min, epsilon_decay = 1.0, 0.01, 0.999 # the chance we take a random action to explore (exploration vs exploitation). The min value for epsilon (can't go below this). What to multiply epsilon by after each simulation
-episodes = 10000 # number of simulations to run where it will learn
-max_steps = 500 # max length of simulation, if we haven't failed by t=500 we've 'solved' it.
-
-rewards = []
-
-# Clips each feature down to [low_i, high_i] to avoid indexing errors or weird rap arounds, mostly with velocities.
-def clip_obs(obs: np.ndarray, low: np.ndarray, high: np.ndarray) -> np.ndarray:
-    '''
-    Clips each feature of obs into range [low_i, high_i]. Important to avoid wrap arounds.
-    
-    Args:
-        obs (np.ndarray): Array of observations, shape (n_features) or greater.
-        low (np.ndarray): Array of lower bounds, same shape as obs.
-        high (np.ndarray): Array of upper bounds, same shape as obs.
-    
-    Returns:
-        np.ndarray: The clipped array of observations
-    '''
-    return np.clip(obs, low, high)
-
-# Compute moving average to smooth data for plotting
-def moving_average(data: Sequence[float], window_size: int=100) -> np.ndarray:
-    """
-    Computes the moving average of a 1D sequence.
-
-    Args:
-        data (Sequence[float]): A sequence of numeric values.
-        window_size (int): The size of the moving window.
-
-    Returns:
-        np.ndarray: Array of the moving average, length = len(data) - window_size + 1.
-    """
-    return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
-
-for episode in range(episodes):
-    obs, _ = env.reset()
-    obs = clip_obs(obs, lower_bounds, upper_bounds)
-    state = disc.discretize(obs)
-    total_reward = 0 
-    done = False
-    
-    for _ in range(max_steps):
-        # with probability epsilon, take a random action.
-        if np.random.random() < epsilon:
-            action = env.action_space.sample() # get a random action
-        else:
-            action = np.argmax(Q[state]) # do the action that has the best expected reward
-        
-        # Now that we've taken an action, we apply that to the environment and observe the results
-        next_obs, reward, terminated, truncated, _ = env.step(action)
-        done = terminated or truncated # either of these ends the episode
-        # discretize the new state so we can process it
-        next_obs = clip_obs(next_obs, lower_bounds, upper_bounds)
-        next_state = disc.discretize(next_obs)
-        
-        # Now we use the bellman equation to update the q values
-        best_future_q = np.max(Q[next_state]) # This is max a' Q(s', a')
-        td_target = reward + gamma * best_future_q # this is our target value
-        td_error = td_target - Q[state][action] # this is our temporal difference (reality and what we believed)
-        Q[state][action] += alpha * td_error # Update Q-val
-        
-        state = next_state # update our sate
-        total_reward += reward
-        
-        if done:
-            break
-    
-    alpha = max(alpha_min, alpha * alpha_decay)
-    epsilon = max(epsilon_min, epsilon * epsilon_decay)
-    
-    rewards.append(total_reward)
-    if episode % 1000 == 0:
-        print(f'Episode {episode}, total reward: {total_reward}, Epsilon: {epsilon:.3f}, Alpha: {alpha:.3f}')
+cfg = Config()
 
 def moving_average(data: Sequence[float], window_size: int = 100) -> np.ndarray:
     """
@@ -112,20 +24,90 @@ def moving_average(data: Sequence[float], window_size: int = 100) -> np.ndarray:
     """
     return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
 
-print(f'average rewards: {sum(rewards)/len(rewards)}')
+def clip_obs(obs: np.ndarray, low: np.ndarray, high: np.ndarray) -> np.ndarray:
+    '''
+    Clips each feature of obs into range [low_i, high_i]. Important to avoid wrap arounds.
+    
+    Args:
+        obs (np.ndarray): Array of observations, shape (n_features) or greater.
+        low (np.ndarray): Array of lower bounds, same shape as obs.
+        high (np.ndarray): Array of upper bounds, same shape as obs.
+    
+    Returns:
+        np.ndarray: The clipped array of observations
+    '''
+    return np.clip(obs, low, high)
 
-smoothed_rewards = moving_average(rewards)
-plt.plot(smoothed_rewards)
-plt.xlabel('Episodes')
-plt.ylabel('Reward')
-plt.title('Q-learning cartpole reward graph')
-plt.grid()
-plt.show()
+def decay(val: float, decay_rate: float, min_val: float) -> float:
+    return max(val * decay_rate, min_val)
 
-later_rewards = smoothed_rewards[:5000]
-plt.plot(later_rewards)
-plt.xlabel('Episodes (+5000)')
-plt.ylabel('Reward')
-plt.title('later rewards in cartpole')
-plt.grid()
-plt.show()
+def main(cfg: Config):
+    env = gym.make(cfg.env_id)
+    env.observation_space.seed(cfg.seed)
+    env.action_space.seed(cfg.seed)
+    env.action_space.n = 2  # discrete two-action CartPole
+    
+    bins = [cfg.n_bins, cfg.n_bins, cfg.n_angle_bins, cfg.n_bins]
+    Q = np.zeros(bins + [env.action_space.n], dtype=float)
+    disc = Discretizer(bins_per_feature=bins, lower_bounds=cfg.lower_bounds, upper_bounds=cfg.upper_bounds)
+    
+    epsilon = cfg.epsilon_tabular
+    alpha = cfg.alpha_tabular
+    episode_rewards = []
+    
+    print('Starting tabular Q-learning')
+    for episode in range(cfg.episodes_tabular):
+        obs, _ = env.reset()
+        obs = clip_obs(obs, cfg.lower_bounds, cfg.upper_bounds)
+        state = disc.discretize(obs)
+        total_reward = 0.0
+        
+        for _ in range(cfg.max_steps_tabular):
+                if np.random.random() < epsilon:
+                    action = env.action_space.sample()
+                else:
+                    action = np.argmax(Q[state])
+                
+                next_obs, reward, terminated, truncated, _ = env.step(action)
+                done = terminated or truncated
+                next_obs = clip_obs(next_obs, cfg.lower_bounds, cfg.upper_bounds)
+                next_state = disc.discretize(next_obs)
+                
+                # Bellman update
+                td_target = reward + cfg.gamma_tabular * np.max(Q[next_state])
+                td_error = td_target - Q[state][action]
+                Q[state][action] += alpha * td_error
+                
+                state = next_state
+                total_reward += reward
+                
+                if done:
+                    break
+                
+            # decay schedules
+    alpha = decay(alpha, cfg.alpha_decay_tabular, cfg.alpha_min_tabular)
+    epsilon = decay(epsilon, cfg.epsilon_decay_tabular, cfg.epsilon_min_tabular)
+        
+    episode_rewards.append(total_reward)
+    if episode % 1000 == 0:
+        print(f"Episode {episode:5d}  Reward={total_reward:.1f}, epsilon={epsilon:.3f}  alpha={alpha:.3f}")
+        
+    avg_reward = sum(episode_rewards)/(len(episode_rewards))
+    print(f"Average reward over {cfg.episodes_tabular} episodes: {avg_reward:.2f}")
+    plot_rewards(episode_rewards, cfg.window_tabular)
+
+
+def plot_rewards(episode_rewards, window_dqn):
+    smoothed = moving_average(episode_rewards, window_dqn)
+    
+    plt.subplot(1,2,2)
+    plt.plot(smoothed, label=f"{window_dqn}-episode MA")
+    plt.xlabel("Episode")
+    plt.ylabel("Avg Reward")
+    plt.title("Smoothed Reward Curve")
+    plt.legend()
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
--- config.py
+++ config.py
@@ -1,20 +1,43 @@
 from dataclasses import dataclass
+import numpy as np
 
 @dataclass
 class Config:
+    # General
     env_id: str = 'CartPole-v1'
     seed: int = 42
+    
+    # DQN Hyperparameters
     buffer_size: int = 200_000
     batch_size: int = 256
-    gamma: float = 0.99
-    alpha: float = 0.0001
-    alpha_decay: float = 1
-    alpha_min: float = 0
-    epsilon: float = 1
-    epsilon_min: float = 0.01
-    epsilon_decay: float = 0.992
+    gamma_dqn: float = 0.99
+    alpha_dqn: float = 0.0001
+    alpha_decay_dqn: float = 1
+    alpha_min_dqn: float = 0
+    epsilon_dqn: float = 1
+    epsilon_min_dqn: float = 0.01
+    epsilon_decay_dqn: float = 0.992
+    episodes_dqn: int = 2_500
     target_update_freq: int = 10
-    episodes: int = 2_500
     max_steps: int = 500
-    window: int = 100
+    window_dqn: int = 100
     save_path: str = 'dqn_cartpole_solved.pth'
+    warmup: int = 1000
+    
+    # Tabular Q-learning hyperparameters
+    n_bins: int = 10
+    n_angle_bins: int = 20
+    lower_bounds: np.ndarray = np.array([-4.8, -5.0, -0.418, -5.0])
+    upper_bounds: np.ndarray = np.array([ 4.8,  5.0,  0.418,  5.0])
+    gamma_tabular: float = 0.99
+    alpha_tabular: float = 0.3
+    alpha_decay_tabular: float = 0.999
+    alpha_min_tabular: float = 0.005
+    epsilon_tabular: float = 1
+    epsilon_decay_tabular: float = 0.999
+    epsilon_min_tabular: float = 0.01
+    epssiodes_tabular: int = 10_000
+    max_steps_tabular: int = 500
+    window_tabular: int = 100
+    
+    
